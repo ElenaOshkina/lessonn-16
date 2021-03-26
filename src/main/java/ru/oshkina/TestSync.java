794a01778d9b6c88f63d8888ec4e858c59c097ea
Elena Oshkina <eoshkina@dev.vtb>
2021-03-26 09:25:22 +0300

    add TestSyc

--- src/main/java/ru/oshkina/TestSync.java
+++ src/main/java/ru/oshkina/TestSync.java
@@ -1,19 +1,19 @@
 package ru.oshkina;
 
 public class TestSync implements Runnable {
-    private int balance = 1; //todo: подумать как сделать так, чтобы переменная не кешировалась в каждом потоке
+    private volatile int balance = 1; //todo: подумать как сделать так, чтобы переменная не кешировалась в каждом потоке
 
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
             increment();
-            System.out.println("balance = " + balance);
         }
     }
 
     public synchronized void increment() {
         int temp = balance;//a 10
         balance = temp * 2;//b 11
+        System.out.println("balance = " + balance + " " + Thread.currentThread().getName());
     }
 
 
